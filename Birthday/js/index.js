fcf2a5f2027f9993da07d420236ee7c2ca18c29b
Guo Heng <604816670@qq.com>
2022-03-28 09:36:02 +0800

    更换

--- Birthday/js/index.js
+++ Birthday/js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('hi|夏乐小仙女|祝你|生日快乐|happy birthday|#rectangle|#countdown 3||');
+      S.UI.simulate('hi|小仙女|祝你|生日快乐|happy birthday|#rectangle|#countdown 3||');
     }
 
     S.Drawing.loop(function () {
